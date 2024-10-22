04fd6273725c1f03f8c921338ab9f0c5faf83538
Iván Ibáñez™ <ivan01092004@gmail.com>
2024-10-23 00:16:28 +0200

    Pantalla Register

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:pruebas_vila_explorer/src/pages/register_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Oculta la banda de "debug"
+      title: 'Vila Explorer',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: RegisterPage(),
+    );
+  }
+}
